36913986b2ffc897bf5b119b8c8fcb18bfca6198
anotherbugmaster <anotherbugmaster@gmail.com>
2014-11-25 00:13:56 +0500

    At least in some cases calendar works.

--- Calendar/MainWindow.xaml.cs
+++ Calendar/MainWindow.xaml.cs
@@ -20,17 +20,45 @@ namespace Calendar
 
         private void InitializeBehaviour()
         {
-            var datesBlocks = Enumerable.Range(0, VisualTreeHelper.GetChildrenCount(Grid))
+            //TODO: refactor all the mess below!
+            var daysBlocks = Enumerable.Range(0, VisualTreeHelper.GetChildrenCount(Grid))
                                         .Select(index => VisualTreeHelper.GetChild(Grid, index) as TextBlock)
                                         .Where(item => item != null && item.Text == "0")
                                         .Select((item, index) => new {TextBlock = item, Index = index});
-            foreach (var item in datesBlocks)
+
+
+            DateTime now = DateTime.Now;
+            CurrentMonth.Text = now.ToString("MMMM yyyy");
+            DateTime firstDayOfMonth = new DateTime(now.Year, now.Month, 1);
+
+            foreach (var item in daysBlocks)
             {
                 item.TextBlock.Tag = item.Index;
-                item.TextBlock.Text = (item.Index + 1).ToString();
+                var dayNumber = "";
+                //TODO: fix magic constants!
+                if (item.Index > (int)firstDayOfMonth.DayOfWeek - 2 && 
+                    item.Index <= DateTime.DaysInMonth(now.Year, now.Month) + (int)firstDayOfMonth.DayOfWeek - 2)
+                    dayNumber = (item.Index - (int)firstDayOfMonth.DayOfWeek + 2).ToString();
+                item.TextBlock.Text = dayNumber;
             }
-            DateTime date = DateTime.Now;
-            CurrentMonth.Text = date.ToString("MMMM yyyy");
+
+            var weeksBlocks = Enumerable.Range(0, VisualTreeHelper.GetChildrenCount(Grid))
+                                        .Select(index => VisualTreeHelper.GetChild(Grid, index) as TextBlock)
+                                        .Where(item => item != null && item.Text == "w")
+                                        .Select((item, index) => new { TextBlock = item, Index = index });
+
+            foreach (var item in weeksBlocks)
+            {
+                item.TextBlock.Tag = item.Index;
+                var weekNumber = item.Index.ToString();
+                item.TextBlock.Text = weekNumber;
+            }
+
+            //Sorry, little circle.
+            Grid.SetRow(UglyCircle, (now.Day + (int)firstDayOfMonth.DayOfWeek) / 7 + 2);
+            Grid.SetColumn(UglyCircle, (int)now.DayOfWeek);
+
+            Grid.SetZIndex(UglyCircle, 0);
         }
     }
 }
