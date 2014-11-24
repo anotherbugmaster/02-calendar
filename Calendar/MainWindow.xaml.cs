2d17822d686c274322e6c6f7dbb6fc1547886755
anotherbugmaster <anotherbugmaster@gmail.com>
2014-11-24 14:18:57 +0500

    Made basic WPF skeleton.

---
+++ Calendar/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls.Primitives;
+
+namespace Calendar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeBehaviour();
+        }
+
+        private void InitializeBehaviour()
+        {
+
+        }
+
+
+    }
+}
