258685c86c1645631f77cac57f53d73e472c7d4b
anotherbugmaster <anotherbugmaster@gmail.com>
2014-11-25 15:07:37 +0500

    Primitive decomposition applied

--- Calendar/MainWindow.xaml.cs
+++ Calendar/MainWindow.xaml.cs
@@ -22,33 +22,45 @@ namespace Calendar
 
         private void InitializeBehaviour()
         {
-            string[] args = Environment.GetCommandLineArgs();
-            var time = new DateTime();
-            if (args.Length > 1)
-                time = DateTime.ParseExact(args[1], "dd.MM.yyyy", CultureInfo.CurrentCulture);
-            else
-                time = DateTime.Now;
-            //TODO: refactor all the mess below!
+            var time = ParseConsoleDate();
+            DrawCalendar(time);
+        }
+
+        private int GetRusDayOfWeek(DayOfWeek day)
+        {
+            return (7 + (int) day - 1) % 7;
+        }
+
+        private void PlaceDateCircle(DateTime firstDayOfMonth, DateTime time)
+        {
+            //Sorry, little circle.
+            Grid.SetRow(UglyCircle, (GetRusDayOfWeek(firstDayOfMonth.DayOfWeek) + time.Day - 1) / 7 + 2);
+            Grid.SetColumn(UglyCircle, GetRusDayOfWeek(time.DayOfWeek) + 1);
+            Grid.SetZIndex(UglyCircle, 0);
+        }
+
+        private void PlaceDateBlocks(DateTime firstDayOfMonth, DateTime time)
+        {
             var daysBlocks = Enumerable.Range(0, VisualTreeHelper.GetChildrenCount(Grid))
                                         .Select(index => VisualTreeHelper.GetChild(Grid, index) as TextBlock)
                                         .Where(item => item != null && item.Text == "0")
-                                        .Select((item, index) => new {TextBlock = item, Index = index});
+                                        .Select((item, index) => new { TextBlock = item, Index = index });
 
-            CurrentMonth.Text = time.ToString("MMMM yyyy");
-            DateTime firstDayOfMonth = new DateTime(time.Year, time.Month, 1);
-            DateTime firstDayOfYear = new DateTime(time.Year, 1, 1);
 
             foreach (var item in daysBlocks)
             {
                 item.TextBlock.Tag = item.Index;
                 var dayNumber = "";
                 //TODO: fix magic constants!
-                if (item.Index > GetRusDayOfWeek(firstDayOfMonth.DayOfWeek) - 1 && 
+                if (item.Index > GetRusDayOfWeek(firstDayOfMonth.DayOfWeek) - 1 &&
                     item.Index <= DateTime.DaysInMonth(time.Year, time.Month) + GetRusDayOfWeek(firstDayOfMonth.DayOfWeek) - 1)
                     dayNumber = (item.Index - GetRusDayOfWeek(firstDayOfMonth.DayOfWeek) + 1).ToString();
                 item.TextBlock.Text = dayNumber;
             }
+        }
 
+        private void PlaceWeekBlocks(DateTime firstDayOfMonth, DateTime firstDayOfYear)
+        {
             var weeksBlocks = Enumerable.Range(0, VisualTreeHelper.GetChildrenCount(Grid))
                                         .Select(index => VisualTreeHelper.GetChild(Grid, index) as TextBlock)
                                         .Where(item => item != null && item.Text == "w")
@@ -61,17 +73,30 @@ namespace Calendar
                 var weekNumber = ((firstDayOfMonth.DayOfYear - GetRusDayOfWeek(firstDayOfYear.DayOfWeek)) / 7 + item.Index) % 52 + 1;
                 item.TextBlock.Text = weekNumber.ToString();
             }
-
-            //Sorry, little circle.
-            Grid.SetRow(UglyCircle, (GetRusDayOfWeek(firstDayOfMonth.DayOfWeek) + time.Day - 1) / 7 + 2);
-            Grid.SetColumn(UglyCircle, GetRusDayOfWeek(time.DayOfWeek) + 1);
-
-            Grid.SetZIndex(UglyCircle, 0);
         }
 
-        private int GetRusDayOfWeek(DayOfWeek day)
+        private void DrawCalendar(DateTime time)
         {
-            return (7 + (int) day - 1) % 7;
+            CurrentMonth.Text = time.ToString("MMMM yyyy");
+
+            DateTime firstDayOfMonth = new DateTime(time.Year, time.Month, 1);
+            DateTime firstDayOfYear = new DateTime(time.Year, 1, 1);
+
+            PlaceDateBlocks(firstDayOfMonth, time);
+            PlaceWeekBlocks(firstDayOfMonth, firstDayOfYear);
+            PlaceDateCircle(firstDayOfMonth, time);
+        }
+
+        private DateTime ParseConsoleDate()
+        {
+
+            string[] args = Environment.GetCommandLineArgs();
+            var time = new DateTime();
+            if (args.Length > 1)
+                time = DateTime.ParseExact(args[1], "dd.MM.yyyy", CultureInfo.CurrentCulture);
+            else
+                time = DateTime.Now;
+            return time;
         }
     }
 }
